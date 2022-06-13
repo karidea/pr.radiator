5a99cd8d56cf816d398ae67dbe4ffa956de83a2e
karidea <60665031+karidea@users.noreply.github.com>
2022-06-13 11:48:00 -0500

    Remove test and update libraries to latest

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders settings-form', () => {
-  const { getByText } = render(<App />);
-  const headerElement = getByText(/Configure PR Radiator/i);
-  expect(headerElement).toBeInTheDocument();
-});
